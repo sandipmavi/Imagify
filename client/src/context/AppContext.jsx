c0af9727637d94b01b421cf29dd37c574635b4f6
sandipmavi <gurjarsandeep9897@gmail.com>
2025-07-19 14:01:07 +0530

    Added Nav

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
+const AppContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const value = {
+    user,
+    setUser,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+export default AppContextProvider;
