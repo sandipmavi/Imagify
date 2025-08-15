071b13b24c579a90cfb25bd5fc59353e283f348b
sandipmavi <gurjarsandeep9897@gmail.com>
2025-08-15 14:26:58 +0530

    implemented landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "../components/Header";
+import Steps from "../components/Steps";
+import Description from "../components/Description";
+import Testimonials from "../components/Testimonials";
+import GenerateBtn from "../components/GenerateBtn";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <Steps />
+      <Description />
+      <Testimonials />
+      <GenerateBtn />
+    </div>
+  );
 };
 
 export default Home;
