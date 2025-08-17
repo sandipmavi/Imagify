0fded6a8ae28c6e1449f2d1c13cdc379e3d54161
sandipmavi <gurjarsandeep9897@gmail.com>
2025-08-17 17:00:12 +0530

    Updating app

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,11 +4,19 @@ export const AppContext = createContext();
 const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [credit, setCredit] = useState(false);
   const value = {
     user,
     setUser,
     showLogin,
     setShowLogin,
+    backendUrl,
+    token,
+    setToken,
+    credit,
+    setCredit,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
